1a0186ad764621abb50941d56a59c38b153b931d
Trent <garciabruno@live.com.ar>
2016-05-15 04:58:36 -0300

    Removed json dumps and decorated functions with as_json

--- blueprints/ajax/store/cart.py
+++ blueprints/ajax/store/cart.py
@@ -3,62 +3,76 @@
 
 from flask import request
 from flask import Blueprint
+from flask import render_template
 
 from steamcommerce_api.api import cart
+from steamcommerce_api.api import user
 from steamcommerce_api.api import product
 
+from utils import route_decorators
 from inputs import store_inputs
-
-import json
 
 
 ajax_cart = Blueprint('ajax.store.cart', __name__)
 
 
 @ajax_cart.route('/add/', methods=['POST'])
+@route_decorators.as_json
 def ajax_cart_add():
     user_id = 1
     form = store_inputs.AddCartInput(request)
 
     if not form.validate():
-        return json.dumps({'success': False, 'errors': form.errors}), 422
+        return ({'success': False, 'status': 0}, 422)
 
     product_id = int(request.form.get('product_id'))
     form_product = product.Product().get_product_by_id(product_id)
 
-    if not form_product.get('visible') or not form_product.get('price', None):
-        return json.dumps(
-            {'success': False, 'errors': 'product_not_available'}
-        ), 500
+    if not form_product.get('visible'):
+        return ({'success': False, 'status': 1}, 500)
+
+    if not form_product.get('price'):
+        return ({'success': False, 'status': 2}, 500)
+
+    if form_product.get('product_type') == 2:
+        return ({'success': False, 'status': 3}, 500)
 
     user_cart = cart.Cart().get_user_cart(user_id, trim=True)
 
     if len(user_cart.get('items')) >= 10:
-        return json.dumps(
-            {'success': False, 'errors': 'cart_full'}
-        ), 500
+        return ({'success': False, 'status': 4}, 500)
 
     success = cart.Cart().add_to_user_cart(user_id, product_id)
 
-    if success:
-        return json.dumps(
-            {'success': True, 'cart': len(user_cart.get('items')) + 1}
-        )
+    if not success:
+        return ({'success': False, 'status': 5}, 500)
 
-    return json.dumps(
-        {'success': False, 'errors': 'unknown'}, 500
-    )
+    cart_items_count = len(user_cart.get('items'))
+    return {'success': True, 'cart': cart_items_count + 1}
 
 
 @ajax_cart.route('/remove/', methods=['POST'])
+@route_decorators.as_json
 def ajax_cart_remove():
     user_id = 1
     form = store_inputs.RemoveCartInput(request)
 
     if not form.validate():
-        return json.dumps({'success': True, 'errors': form.errors})
+        return ({'success': True, 'errors': form.errors}, 422)
 
     relation_id = int(request.form.get('relation_id'))
     success = cart.Cart().remove_from_user_cart(user_id, relation_id)
 
-    return json.dumps({'success': bool(success)})
+    cart.Cart().process_cart(user_id)
+
+    user_data = user.User().get_by_id(user_id)
+    user_cart = cart.Cart().get_user_cart(user_id)
+
+    template_params = {
+        'user': user_data,
+        'user_cart': user_cart
+    }
+
+    hud = render_template('views/cart/hud.html', **template_params)
+
+    return {'success': bool(success), 'hud': hud}
