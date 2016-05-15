66a88905bb401d72b3b93c621e0912d82f2deaa5
Trent <garciabruno@live.com.ar>
2016-05-15 04:59:25 -0300

    Removed json dumps and decorated functions with as_json

--- blueprints/ajax/store/userrequest.py
+++ blueprints/ajax/store/userrequest.py
@@ -4,18 +4,22 @@
 from flask import request
 from flask import Blueprint
 
-from inputs import store_inputs
-
-import json
-
 from steamcommerce_api.api import cart
 from steamcommerce_api.api import product
+from steamcommerce_api.api import history
 from steamcommerce_api.api import userrequest
+
+from inputs import store_inputs
+from utils import route_decorators
+
+import constants
+
 
 ajax_userrequest = Blueprint('ajax.store.userrequest', __name__)
 
 
 @ajax_userrequest.route('/generate/', methods=['POST'])
+@route_decorators.as_json
 def ajax_userrequest_generate():
     user_id = 1
     email = 'admin@extremegaming-arg.com.ar'
@@ -23,19 +27,22 @@ def ajax_userrequest_generate():
     form = store_inputs.UserRequestInput(request)
 
     if not form.validate():
-        return json.dumps({'success': False, 'status': 0}), 422
+        return ({'success': False, 'status': 0}, 422)
 
     product_id = int(request.form.get('product_id'))
     product_data = product.Product().get_product_by_id(product_id)
 
     if product_data is None:
-        return json.dumps({'success': False, 'status': 1}), 500
+        return ({'success': False, 'status': 1}, 500)
 
     if not product_data.get('visible'):
-        return json.dumps({'success': False, 'status': 2}), 500
+        return ({'success': False, 'status': 2}, 500)
 
     if not product_data.get('price'):
-        return json.dumps({'success': False, 'status': 3}), 500
+        return ({'success': False, 'status': 3}, 500)
+
+    if product_data.get('price').get('price_type') != 1:
+        return ({'success': False, 'status': 4}, 500)
 
     price = product_data.get('price').get('price')
     promotion = True if product_data.get('promotion') else False
@@ -52,15 +59,22 @@ def ajax_userrequest_generate():
         # TODO: Delete UserRequest generated from user
 
         if invoice.get('status') == 0:
-            return json.dumps({'success': False, 'status': 5}), 500
+            return ({'success': False, 'status': 6}, 500)
 
         if invoice.get('status') == 1:
-            return json.dumps({'success': False, 'status': 4}), 500
+            return ({'success': False, 'status': 5}, 500)
 
-    return json.dumps(invoice)
+    history.History().push(
+        constants.HISTORY_GENERATED_STATE,
+        invoice.get('userrequest'),
+        constants.HISTORY_USERREQUEST_TYPE
+    )
+
+    return invoice
 
 
 @ajax_userrequest.route('/cart/generate/')
+@route_decorators.as_json
 def ajax_userrequest_cart_generate():
     user_id = 1
     email = 'admin@extremegaming-arg.com.ar'
@@ -69,10 +83,10 @@ def ajax_userrequest_cart_generate():
     user_cart = cart.Cart().get_user_cart(user_id)
 
     if len(user_cart.get('items')) == 0:
-        return json.dumps({'success': False, 'status': 0}), 500
+        return ({'success': False, 'status': 0}, 500)
 
     if not user_cart.get('prices').get('normal'):
-        return json.dumps({'success': False, 'status': 1}), 500
+        return ({'success': False, 'status': 1}, 500)
 
     cart_items = user_cart.get('items')
 
@@ -92,12 +106,12 @@ def ajax_userrequest_cart_generate():
         # TODO: Delete UserRequest generated from user
 
         if invoice.get('status') == 0:
-            return json.dumps({'success': False, 'status': 3}), 500
+            return ({'success': False, 'status': 3}, 500)
 
         if invoice.get('status') == 1:
-            return json.dumps({'success': False, 'status': 4}), 500
+            return ({'success': False, 'status': 4}, 500)
 
     cart.Cart().empty_user_cart(user_id)
     invoice.update({'promotional': promotion})
 
-    return json.dumps(invoice)
+    return invoice
