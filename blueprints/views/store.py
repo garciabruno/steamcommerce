f4285e0902d00356f6018a0fd645531348706b72
Trent <garciabruno@live.com.ar>
2016-05-15 05:00:12 -0300

    Added offers route, added temporary login routes

--- blueprints/views/store.py
+++ blueprints/views/store.py
@@ -5,8 +5,14 @@
 External imports
 '''
 
+from flask import redirect
+from flask import session
 from flask import Blueprint
 from flask import render_template
+
+'''
+Internal imports
+'''
 
 from steamcommerce_api.api import user
 from steamcommerce_api.api import slider
@@ -17,21 +23,35 @@ from steamcommerce_api.api import storepromotion
 store = Blueprint('views.store', __name__)
 
 
+@store.route('/login')
+def store_login():
+    session['user'] = 1
+    session['admin'] = True
+
+    return redirect('/')
+
+
+@store.route('/logout')
+def store_logout():
+    if session.get('user'):
+        session.pop('user')
+
+    if session.get('admin'):
+        session.pop('admin')
+
+    return redirect('/')
+
+
+@store.route('/comprar')
+def store_old_route():
+    return redirect('/')
+
+
 @store.route('/')
-def store_root():
-    return render_template('views/store/landing.html')
-
-
-@store.route('/tienda/')
-@store.route('/catalogo/')
-@store.route('/comprar/')
-@store.route('/tienda/pagina/<int:page_id>')
-@store.route('/catalogo/pagina/<int:page_id>')
-@store.route('/comprar/pagina/<int:page_id>')
-def store_catalog(page_id=1):
+def store_catalog():
     sliders = slider.Slider().get_active()
     sections = section.Section().get_active()
-    products = product.Product().get_by_section(sections[0].id, page_id)
+    products = product.Product().get_by_section(sections[0].id, 1)
     promotions = storepromotion.StorePromotion().get_active_promotions()
     promotion_products = []
 
@@ -44,7 +64,30 @@ def store_catalog(page_id=1):
         'products': products,
         'sections': sections,
         'promotions': promotions,
+        'active_section': 'catalog',
         'promotion_products': promotion_products
+    }
+
+    return render_template('views/store/catalog.html', **template_params)
+
+
+@store.route('/ofertas')
+def store_offers():
+    spromotion = storepromotion.StorePromotion()
+    promotions = spromotion.get_active_promotions()
+    promotion_products = []
+
+    if len(promotions) > 0:
+        promotion_products = spromotion.get_promotion_products(
+            promotions[0].id,
+            1
+        )
+
+    template_params = {
+        'sections': promotions,
+        'promotions': promotions,
+        'active_section': 'offers',
+        'promotion_products': promotion_products,
     }
 
     return render_template('views/store/catalog.html', **template_params)
