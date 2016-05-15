056460e80b22f60468946a1b6d67611eb377409a
Trent <garciabruno@live.com.ar>
2016-05-15 04:59:43 -0300

    Style

--- blueprints/views/cart.py
+++ blueprints/views/cart.py
@@ -15,8 +15,8 @@ def cart_root():
     user_id = 1
 
     cart.Cart().process_cart(user_id)
-    user_cart = cart.Cart().get_user_cart(user_id)
 
+    user_cart = cart.Cart().get_user_cart(user_id)
     user_data = user.User().get_by_id(user_id)
 
     params = {
