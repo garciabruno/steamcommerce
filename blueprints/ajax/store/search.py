50406b96a27f34f8834025a68183fbef435a4b83
Trent <garciabruno@live.com.ar>
2016-05-15 04:59:13 -0300

    Removed json dumps and decorated functions with as_json

--- blueprints/ajax/store/search.py
+++ blueprints/ajax/store/search.py
@@ -8,22 +8,22 @@ from flask import render_template
 from steamcommerce_api.api import product
 
 from inputs import store_inputs
-
-import json
+from utils import route_decorators
 
 ajax_search = Blueprint('ajax.store.search', __name__)
 
 
 @ajax_search.route('/products/', methods=['POST'])
+@route_decorators.as_json
 def ajax_search_products():
     form = store_inputs.SearchInput(request)
 
     if not form.validate():
-        return json.dumps({'success': False, 'errors': form.errors}), 422
+        return ({'success': False, 'errors': form.errors}, 422)
 
     matches = product.Product().search_by_title(request.form.get('title'))
 
     if len(matches) == 0:
-        return json.dumps({'success': False, 'status': 0}), 500
+        return ({'success': False, 'status': 0}, 500)
 
     return render_template('views/store/products.html', products=matches)
