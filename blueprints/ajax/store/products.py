e1e7b19565265f5c1c5b702cc4a0f613b7e4e165
Bruno <garcia.bruno0@gmail.com>
2016-12-17 20:54:11 -0300

    Added shipping routes

--- blueprints/ajax/store/products.py
+++ blueprints/ajax/store/products.py
@@ -2,10 +2,15 @@
 # -*- coding:Utf-8 -*-
 
 from flask import abort
+from flask import request
 from flask import Blueprint
 from flask import render_template
 
 from steamcommerce_api.api import product
+from steamcommerce_api.core import models
+
+import config
+from utils import route_decorators
 
 ajax_products = Blueprint('ajax.store.products', __name__)
 
@@ -21,3 +26,55 @@ def ajax_store_products_page(section, page_id):
         )
     else:
         return abort(404)
+
+
+@ajax_products.route('/cities/', methods=['POST'])
+@route_decorators.as_json
+def ajax_store_cities():
+    letter = request.form.get('letter')
+
+    if not letter or not len(letter):
+        return []
+
+    cities = models.City.select().where(
+        models.City.province_letter == letter
+    ).order_by(
+        models.City.name.asc()
+    )
+
+    return [
+        {'internal_id': x.internal_id, 'name': x.name} for x in cities
+    ]
+
+
+@ajax_products.route('/shipping/price/', methods=['POST'])
+@route_decorators.as_json
+def ajax_shipping_price():
+    city_id = request.form.get('city_id')
+    province_letter = request.form.get('province_letter')
+
+    if not province_letter or not city_id:
+        return ({'success': False, 'status': 0}, 500)
+
+    try:
+        city_id = int(city_id)
+    except:
+        return ({'success': False, 'status': 0}, 500)
+
+    try:
+        models.Province.get(letter=province_letter)
+    except:
+        return ({'success': False, 'status': 1}, 500)
+
+    try:
+        models.City.get(internal_id=city_id)
+    except:
+        return ({'success': False, 'status': 2}, 500)
+
+    if province_letter == 'X' and city_id == 26120:
+        if request.form.get('pickup'):
+            return {'success': True, 'price': config.SHIPPING_TIER_0}
+        else:
+            return {'success': True, 'price': config.SHIPPING_TIER_1}
+
+    return {'success': True, 'price': config.SHIPPING_TIER_2}
