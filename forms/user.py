90d62bf4bb323705e2e4376689cf96119b5d3294
Bruno <garcia.bruno0@gmail.com>
2016-12-17 20:55:09 -0300

    Added shipping form

--- forms/user.py
+++ forms/user.py
@@ -7,13 +7,25 @@ import constants
 
 from wtforms.validators import Email
 from wtforms.validators import Length
+from wtforms.validators import Optional
 from wtforms.validators import DataRequired
-# from wtforms.validators import Optional
 
 from wtforms import TextField
 from wtforms import HiddenField
 from wtforms import SubmitField
+from wtforms import SelectField
 from wtforms import BooleanField
+from wtforms import IntegerField
+
+from steamcommerce_api.core import models
+
+PROVINCE_CHOICES = [('', 'Seleccionar Provincia')]
+
+PROVINCE_CHOICES += [
+    (x.letter, x.name) for x in models.Province.select().order_by(
+        models.Province.name.asc()
+    )
+]
 
 
 class RegisterForm(Form):
@@ -31,12 +43,6 @@ class RegisterForm(Form):
     ]
 
     email = TextField(u'Dirección de email', validators=email_validator)
-
-    # subscribe = BooleanField(
-    #     u'Suscribirse a la lista de emails',
-    #     default=True,
-    #     validators=[Optional()]
-    # )
 
     submit = SubmitField('Enviar', validators=[DataRequired()])
 
@@ -58,11 +64,7 @@ class ProfileForm(Form):
     email = TextField(u'Dirección de email', validators=email_validator)
     name = TextField(u'Nombre', validators=[DataRequired()])
     last_name = TextField(u'Apellido', validators=[DataRequired()])
-    subscribe = BooleanField(
-        u'Suscribirse a la lista de emails',
-        default=True,
-        validators=[DataRequired()]
-    )
+
     submit = SubmitField('Enviar', validators=[DataRequired()])
 
 
@@ -73,3 +75,56 @@ class ReservationForm(Form):
 class TestimonialsForm(Form):
     page_id = HiddenField(u'page_id', validators=[DataRequired()])
     submit = SubmitField('Cargar mas', validators=[DataRequired()])
+
+
+class ShippingForm(Form):
+    userrequest_id = HiddenField(u'userrequest_id')
+    paidrequest_id = HiddenField(u'paidrequest_id')
+
+    first_name = TextField(
+        u'Nombre del destinatario',
+        validators=[DataRequired()]
+    )
+
+    last_name = TextField(
+        u'Apellido del destinatario',
+        validators=[DataRequired()]
+    )
+
+    id_number = IntegerField(
+        u'Número de DNI (Solo números, sin puntos)',
+        validators=[DataRequired()]
+    )
+
+    province = SelectField(
+        u'Provincia',
+        choices=PROVINCE_CHOICES,
+        validators=[DataRequired()]
+    )
+
+    city = SelectField(
+        u'Ciudad',
+        default=None,
+        choices=[],
+        validators=[DataRequired()]
+    )
+
+    address_one = TextField(
+        u'Dirección',
+        validators=[DataRequired()]
+    )
+
+    address_two = TextField(
+        u'Dirección Secundaria (Opcional)',
+        validators=[Optional()]
+    )
+
+    zipcode = TextField(
+        u'Código Postal Argentino',
+        validators=[DataRequired()]
+    )
+
+    submit = SubmitField(
+        u'Finalizar compra',
+        validators=[DataRequired()]
+    )
