d1fca558f6340e54a2744c8dd7b6b0effdea2918
Bruno <garcia.bruno0@gmail.com>
2017-05-03 23:12:16 -0300

    Disabled purchases and invoice generation

--- blueprints/ajax/store/userrequest.py
+++ blueprints/ajax/store/userrequest.py
@@ -27,6 +27,8 @@ ajax_userrequest = Blueprint('ajax.store.userrequest', __name__)
 @route_decorators.ajax_is_logged_in
 @route_decorators.as_json
 def ajax_userrequest_generate():
+    return ({'success': False, 'status': 10}, 500)
+
     curr_user = user.User().get_by_id(session.get('user'))
     form = store_inputs.UserRequestInput(request)
 
@@ -119,6 +121,8 @@ def ajax_userrequest_generate():
 @route_decorators.ajax_is_logged_in
 @route_decorators.as_json
 def ajax_userrequest_cart_generate():
+    return ({'success': False, 'status': 10}, 500)
+
     curr_user = user.User().get_by_id(session.get('user'))
 
     cart.Cart().process_cart(curr_user['id'])
