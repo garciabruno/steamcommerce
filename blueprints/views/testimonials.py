b6442c7352e709d4970f243941f14644821acaf0
Bruno <garcia.bruno0@gmail.com>
2017-08-06 18:56:07 -0300

    Disabled store

--- blueprints/views/testimonials.py
+++ blueprints/views/testimonials.py
@@ -5,85 +5,17 @@
 External imports
 '''
 
-from flask import abort
-from flask import request
-from flask import url_for
 from flask import redirect
 from flask import Blueprint
-from flask import render_template
 
 '''
 Internal imports
 '''
 
-from steamcommerce_api.api import testimonial
-from forms import user as user_form
 
 testimonials = Blueprint('views.testimonials', __name__)
 
 
 @testimonials.route('/', methods=['GET', 'POST'])
 def testimonials_root():
-    if request.method == 'GET':
-        testimonials_data = testimonial.Testimonial().get_active(
-            1,
-            excludes=[
-                'product_specs',
-                'product_codes',
-                'product_tags'
-            ]
-        )
-
-        testimonials_count = testimonial.Testimonial().get_count()
-
-        form = user_form.TestimonialsForm()
-        form.page_id.data = 2
-
-        template_params = {
-            'testimonials': testimonials_data,
-            'active_section': 'testimonials',
-            'count': testimonials_count,
-            'form': form
-        }
-
-        return render_template(
-            'views/testimonials/view.html',
-            **template_params
-        )
-    elif request.method == 'POST':
-        form = user_form.TestimonialsForm(request.form)
-
-        if not form.validate():
-            return redirect(url_for('views.testimonials.testimonials_root'))
-
-        try:
-            int(form.page_id.data)
-        except ValueError:
-            return redirect(url_for('views.testimonials.testimonials_root'))
-
-        testimonials_count = testimonial.Testimonial().get_count()
-        testimonials_data = testimonial.Testimonial().get_active(
-            int(form.page_id.data),
-            excludes=[
-                'product_specs',
-                'product_codes',
-                'product_tags'
-            ]
-        )
-
-        form.page_id.data = int(form.page_id.data)
-        form.page_id.data += 1
-
-        template_params = {
-            'testimonials': testimonials_data,
-            'active_section': 'testimonials',
-            'count': testimonials_count,
-            'form': form
-        }
-
-        return render_template(
-            'views/testimonials/view.html',
-            **template_params
-        )
-    else:
-        return abort(200)
+    return redirect('/')
--- blueprints/views/user.py
+++ blueprints/views/user.py
@@ -8,13 +8,8 @@ from flask import url_for
 from flask import Blueprint
 from flask import render_template
 
-from steamcommerce_tasks.tasks import product as product_task
-
 from steamcommerce_api.api import user
-from steamcommerce_api.api import cart
-from steamcommerce_api.api import crawler
 from steamcommerce_api.api import steam
-from steamcommerce_api.api import product
 from steamcommerce_api.api import userrequest
 from steamcommerce_api.api import paidrequest
 from steamcommerce_api.api import testimonial
@@ -277,13 +272,7 @@ def user_notifications_seen():
 @user_view.route('/testimonios/nuevo')
 @route_decorators.is_logged_in
 def user_testimonials_new():
-    user_id = session.get('user')
-    pending_testimonials = testimonial.Testimonial().get_unsubmited(user_id)
-
-    return render_template(
-        'views/store/new_testimonials.html',
-        pending_testimonials=pending_testimonials
-    )
+    return redirect('/')
 
 
 @user_view.route('/testimonial/skip/', methods=['POST'])
@@ -318,121 +307,25 @@ def user_testimonial_skip():
 @route_decorators.is_logged_in
 @route_decorators.as_json
 def user_testimonial_submit():
-    user_id = session.get('user')
-    testimonial_id = request.form.get('testimonial_id')
-    content = request.form.get('content')
-
-    if not testimonial_id or not content or len(content) < 10:
-        return ({'success': False, 'status': 1}, 500)
-
-    pending_testimonials = testimonial.Testimonial().get_unsubmited(
-        user_id,
-        excludes=['all']
-    )
-
-    testimonial_id = int(testimonial_id)
-
-    if testimonial_id not in [x['id'] for x in pending_testimonials]:
-        return ({'success': False, 'status': 2}, 500)
-
-    testimonial.Testimonial().update(**{
-        'id': testimonial_id,
-        'submited': True,
-        'content': content,
-        'date': datetime.datetime.now()
-    })
-
     return {'success': True}
 
 
 @user_view.route('/wishlist')
 @route_decorators.is_logged_in
 def user_wishlist():
-    user_id = session.get('user')
-    curr_user = user.User().get_by_id(user_id)
-
-    wishlist = crawler.SteamCrawler().get_user_wishlist(curr_user['steam'])
-
-    if len(wishlist) > 0:
-        if wishlist[0] is False:
-            if wishlist[1] == 1:
-                params = {
-                    'title': 'Lista de deseados no disponible',
-                    'content': 'Tu lista de deseados es privada en Steam \
-<br><a target="_blank" href="http://steamcommunity.com/id/me/edit/settings">\
-Editar privacidad en Steam</a><br>\
- <a href="/wishlist/retry">Refrescar lista de deseados</a>'
-                }
-            elif wishlist[1] == 2:
-                params = {
-                    'title': 'Lista de deseados no disponible',
-                    'content': u'No pudimos extraer tu lista de deseados en\
-Steam. ¿Contactá un administrador? \
-<a target="_blank" href="https://facebook.com/ExtremeGamingSTEAM">\
-Facebook ExtremeGaming</a>'
-                }
-
-            return render_template('views/error.html', **params)
-
-    products = []
-
-    for app_id in wishlist[:100]:
-        try:
-            products.append(product.Product().get_app_id(
-                app_id, excludes=[
-                    'product_codes',
-                    'product_tags',
-                    'product_specs'
-                ])
-            )
-        except product.Product().model.DoesNotExist:
-            product_task.add_product_to_store(
-                app_id=app_id
-            )
-
-    params = {
-        'page': 1,
-        'section': 'wishlist',
-        'products': products,
-    }
-
-    return render_template('views/user/wishlist.html', **params)
+    return redirect('/')
 
 
 @user_view.route('/wishlist/retry')
 @route_decorators.is_logged_in
 def user_wishlist_retry():
-    user_id = session.get('user')
-
-    retries = user.User().get_wishlist_retries(user_id)
-
-    if retries >= 5:
-        params = {
-            'title': u'Re-intentos excedidos',
-            'content': u'Has llegado a tu limite de re-intentos de\
- lista de deseados, intentalo nuevo en unas horas'
-        }
-
-        return render_template('views/error.html', **params)
-
-    user.User().purge_user_wishlist(user_id)
-
     return redirect(url_for('views.user.user_wishlist'))
 
 
 @user_view.route('/user/cart/')
 @route_decorators.is_logged_in
 def user_cart():
-    user_id = session.get('user')
-
-    cart.Cart().process_cart(user_id)
-    user_cart = cart.Cart().get_user_cart(user_id)
-
-    params = {
-        'user_cart': user_cart
-    }
-
-    return render_template('views/cart/view.html', **params)
+    return ''
 
 
 @user_view.route('/user/notifications/')
